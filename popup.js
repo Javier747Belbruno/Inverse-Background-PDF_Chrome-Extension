f2af7471dbc5ab456d1adce42b198ffbe09e80df
Javier <javierbelbruno@gmail.com>
2022-01-17 21:08:52 -0300

    add files version 1.0.0

---
+++ popup.js
@@ -0,0 +1,8 @@
+//// popup.js ////
+
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ['content-script.js']
+    });
+});
